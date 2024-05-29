d495a3cabf83f6631847b9f4719c37d07bbf5561
haribonyam <drppr99@gmail.com>
2024-05-29 22:57:32 +0900

    basic security init

---
+++ src/main/java/com/dongle/dongle/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.dongle.dongle.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+        http
+                .authorizeHttpRequests((auth) ->auth
+                        .requestMatchers("/","/login","/join","joinProc","loginProc","/css/**",
+                                "javascript/**").permitAll()
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .requestMatchers("/my/**").hasAnyRole("ADMIN","USER")
+                        .anyRequest().authenticated()
+
+                );
+
+        http
+                .formLogin((auth)->auth.loginPage("/login")
+                        .loginProcessingUrl("/loginPro")
+                        .permitAll()
+                );
+
+
+        //local test 위해서 잠시 비활성화
+        http
+                .csrf((auth)->auth.disable());
+
+
+        return http.build();
+    }
+
+
+}
